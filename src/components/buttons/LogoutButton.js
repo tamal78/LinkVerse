2cdb8750cb912b8ef77cb3191d88480f45b5541b
Tamal <tamalkundu2002@gmail.com>
2024-05-19 00:27:33 +0530

    commit

---
+++ src/components/buttons/LogoutButton.js
@@ -0,0 +1,25 @@
+'use client';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { signOut } from 'next-auth/react';
+
+export default function LogoutButton({
+  className = 'flex items-center gap-2 border p-2 px-4 shadow',
+  iconLeft = false,
+  iconClasses = ''
+}) {
+  return (
+    <button
+      className={className}
+      onClick={() => signOut({ callbackUrl: process.env.NEXT_PUBLIC_URL })}
+    >
+      {iconLeft && (
+        <FontAwesomeIcon icon={faRightFromBracket} className={iconClasses} />
+      )}
+      <span>Logout</span>
+      {!iconLeft && (
+        <FontAwesomeIcon icon={faRightFromBracket} className={iconClasses} />
+      )}
+    </button>
+  );
+}
